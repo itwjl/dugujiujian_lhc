60019b668ec51afd58558208ec18652abb1017fc
wangjinglei <1016879832@qq.com>
2020-03-27 15:21:50 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/jianfa/dugujiujian/Dugu.java
@@ -0,0 +1,12 @@
+package com.atguigu.jianfa.dugujiujian;
+/**
+ *
+ * @author 王敬磊
+ * @email it_wjl@163.com
+ * @date
+ */
+public class Dugu {
+    public static void main(String[] args) {
+        System.out.println("令令令令令令令令令狐冲！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！");
+    }
+}
